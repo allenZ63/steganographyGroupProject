55921bade99ec06149921db1b6b96f1a2a896233
dhw0 <39346736+dhw0@users.noreply.github.com>
2018-05-24 11:57:35 -0700

    Update Main.java

--- src/groupSteganography/Main.java
+++ src/groupSteganography/Main.java
@@ -1,10 +1,22 @@
 package groupSteganography;
 
 public class Main {
-
+	ArrayList<Integer> changes= new ArrayList<Integer>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public void addMesssage (String message) {
+		for (int i=0; i<message.length(); i++) {
+			int temp=message.charAt(i);
+			changes.add(temp);
+		}
+	}
+	public void ModifyAll(ArrayList<Integer> image) {
+		for (int i=0; i<changes.size(); i++) {
+			image.set(i, changes.get(i)+image.get(i));
+		}
 	}
 
 }
